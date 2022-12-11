f58df97dffee16a18d43fc63987beabfefe36e67
Daniel Oyieke <danielligula@gmail.com>
2022-12-11 13:36:57 -0500

    Email Service Cleanup

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -18,10 +18,12 @@ namespace ContactPro.Services
 
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var emailSender =_mailSettings.Email;
+            var emailSender = _mailSettings.Email ?? Environment.GetEnvironmentVariable("Email");
+            
             MimeMessage newEmail = new();
 
             newEmail.Sender = MailboxAddress.Parse(emailSender);
+            
             foreach (var emailAddress in email.Split(";"))
             {
                 newEmail.To.Add(MailboxAddress.Parse(emailAddress));
@@ -29,6 +31,7 @@ namespace ContactPro.Services
             newEmail.Subject = subject;
             
             BodyBuilder emailBody = new();
+            
             emailBody.HtmlBody = htmlMessage;
             
             newEmail.Body = emailBody.ToMessageBody();
@@ -37,9 +40,9 @@ namespace ContactPro.Services
 
             try
             {
-                var host = _mailSettings.Host;
-                var port = _mailSettings.Port;                
-                var password = _mailSettings.Password;
+                var host = _mailSettings.Host ?? Environment.GetEnvironmentVariable("Host");
+                var port = _mailSettings.Port != 0 ? _mailSettings.Port : int.Parse(Environment.GetEnvironmentVariable("Port")!);
+                var password = _mailSettings.Password ?? Environment.GetEnvironmentVariable("Password");
 
                 await smtpClient.ConnectAsync(host, port, SecureSocketOptions.StartTls);
                 await smtpClient.AuthenticateAsync(emailSender, password);
